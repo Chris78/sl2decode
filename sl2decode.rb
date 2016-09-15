a709d7b59e5cdb6c5b7c6283c9eebae1b9419908
pi <pi@localhost>
2016-09-15 19:12:36 +0200

    more conversions added; comments in english now

--- sl2decode.rb
+++ sl2decode.rb
@@ -3,6 +3,8 @@ require 'yaml'
 # Constants:
 POLAR_EARTH_RADIUS = 6356752.3142
 PI = Math::PI
+FT2M = 1/3.2808399  # factor for feet to meter conversions
+KN2KM = 1/1.852     # factor for knots to km conversions
 
 if ARGV[0].to_s==''
   puts "Usage: ruby sl2decode.rb your_file.sl2"
@@ -19,20 +21,20 @@ output = []
 block_offset=0
 
 # 10 Bytes Header
-block_offset+=10   # Startindex des ersten Blocks
+block_offset+=10   # Startindex of the first block
 
 # Datentypen:
 # ===================================================================================================
-# Typ     Definition                                          Direktive für Ruby's String#unpack
+# Type    Definition                                          Directive for Ruby's String#unpack
 # ---------------------------------------------------------------------------------------------------
-# byte 	  UInt8                                                C
-# short 	UInt16LE                                             v
-# int 	  UInt32LE                                             V
-# float 	FloatLE (32 bits IEEE 754 floating point number)     e (od. E?)
-# flags 	UInt16LE                                             v
+# byte 	  UInt8                                               C
+# short   UInt16LE                                            v
+# int 	  UInt32LE                                            V
+# float   FloatLE (32 bits IEEE 754 floating point number)    e
+# flags   UInt16LE                                            v
 # ---------------------------------------------------------------------------------------------------
 
-
+# Available block attributes, their offset inside each block and their type: 
 block_def = {
   'blockSize' => {:offset=>26, :type=>'v'},
 #  'lastBlockSize' => {:offset=>28, :type=>'v'},
@@ -42,15 +44,15 @@ block_def = {
 #  'upperLimit' => {:offset=>38, :type=>'e'},
 #  'lowerLimit' => {:offset=>42, :type=>'e'},
 #  'frequency' => {:offset=>51, :type=>'C'},
-  'waterDepth ' => {:offset=>62, :type=>'e'},   # in feet
-#  'keelDepth ' => {:offset=>66, :type=>'e'},    # in feet
-#  'speedGps' => {:offset=>98, :type=>'e'},      # in knots
-  'temperature' => {:offset=>102, :type=>'e'},  # in °C
+  'waterDepthFt' => {:offset=>62, :type=>'e'},    # in feet
+#  'keelDepthFt' => {:offset=>66, :type=>'e'},    # in feet
+#  'speedGpsKnots' => {:offset=>98, :type=>'e'},  # in knots
+  'temperature' => {:offset=>102, :type=>'e'},    # in °C
   'lowrance_longitude' => {:offset=>106, :type=>'V'},    # Lowrance encoding (easting)
   'lowrance_latitude' => {:offset=>110, :type=>'V'},     # Lowrance encoding (northing)
-#  'speedWater' => {:offset=>114, :type=>'e'},   # Wassergeschwindigkeit (Messrad falls vorhanden, sonst GPS Wert)
+#  'speedWaterKnots' => {:offset=>114, :type=>'e'},   # from "water wheel sensor" if present, else GPS value(?)
 #  'courseOverGround' => {:offset=>118, :type=>'e'},  # ourseOverGround in radians
-#  'altitude ' => {:offset=>122, :type=>'e'},    # in feet
+#  'altitudeFt' => {:offset=>122, :type=>'e'},   # in feet
 #  'heading' => {:offset=>126, :type=>'e'},      # in radians
 #  'flags' => {:offset=>130, :type=>'v'},
 #  'time' => {:offset=>140, :type=>'V'}          # unknown resolution, unknown epoche
@@ -62,9 +64,13 @@ while block_offset<s.length do
   block_def.each do |key,bdef|
     h[key] = s[block_offset+bdef[:offset], block_offset+bdef[:offset]+4].unpack(bdef[:type]).first
   end
-  # Verschiedene Konvertierungen:
-  h['longitude'] = h['lowrance_longitude']/POLAR_EARTH_RADIUS * (180/PI)
-  h['latitude'] = ((2*Math.atan(Math.exp(h['lowrance_latitude']/POLAR_EARTH_RADIUS)))-(PI/2)) * (180/PI)
+  # Some Conversions into non proprietary formats:
+  h['longitude'] = h['lowrance_longitude']/POLAR_EARTH_RADIUS * (180/PI) if h.has_key?('lowrance_longitude')
+  h['latitude'] = ((2*Math.atan(Math.exp(h['lowrance_latitude']/POLAR_EARTH_RADIUS)))-(PI/2)) * (180/PI) if h.has_key?('lowrance_latitude')
+  h['waterDepthM'] = h['waterDepthFt'] * FT2M if h.has_key?('waterDepthFt')
+  h['keelDepthM']  = h['keelDepthFt'] * FT2M if h.has_key?('keelDepthFt')
+  h['altitudeM']   = h['altitudeFt'] * FT2M if h.has_key?('altitudeFt')
+  h['speedGpsKm']  = h['speedGpsKnots'] * KN2KM if h.has_key?('speedGpsKnots')
   begin
     block_offset += h['blockSize']
   rescue 
