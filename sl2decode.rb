215c2130e3135cf59c9ffff03b390c54098ad715
pi <pi@localhost>
2016-09-20 09:32:25 +0200

    much better performance with file seek

--- sl2decode.rb
+++ sl2decode.rb
@@ -12,11 +12,9 @@ if ARGV[0].to_s==''
 end
 
 # Read given sl2 file:
-f=File.open(ARGV[0], 'rb')
-s=f.read(); "#{s.length} Bytes read"
-f.close
-
-output = []
+#f=File.open(ARGV[0], 'rb')
+#s=f.read(); "#{s.length} Bytes read"
+#f.close
 
 block_offset=0
 
@@ -36,34 +34,49 @@ block_offset+=10   # Startindex of the first block
 
 # Available block attributes, their offset inside each block and their type: 
 block_def = {
-  'blockSize' => {:offset=>26, :type=>'v'},
-#  'lastBlockSize' => {:offset=>28, :type=>'v'},
-#  'channel' => {:offset=>30, :type=>'v'},
-#  'packetSize' => {:offset=>32, :type=>'v'},
-#  'frameIndex' => {:offset=>34, :type=>'V'},
-#  'upperLimit' => {:offset=>38, :type=>'e'},
-#  'lowerLimit' => {:offset=>42, :type=>'e'},
-#  'frequency' => {:offset=>51, :type=>'C'},
-  'time1' => {:offset=>58, :type=>'V'},
-  'waterDepthFt' => {:offset=>62, :type=>'e'},    # in feet
-#  'keelDepthFt' => {:offset=>66, :type=>'e'},    # in feet
-#  'speedGpsKnots' => {:offset=>98, :type=>'e'},  # in knots
-  'temperature' => {:offset=>102, :type=>'e'},    # in °C
-  'lowrance_longitude' => {:offset=>106, :type=>'V'},    # Lowrance encoding (easting)
-  'lowrance_latitude' => {:offset=>110, :type=>'V'},     # Lowrance encoding (northing)
-#  'speedWaterKnots' => {:offset=>114, :type=>'e'},   # from "water wheel sensor" if present, else GPS value(?)
-#  'courseOverGround' => {:offset=>118, :type=>'e'},  # courseOverGround in radians
-#  'altitudeFt' => {:offset=>122, :type=>'e'},   # in feet
-#  'heading' => {:offset=>126, :type=>'e'},      # in radians
-   'flags' => {:offset=>130, :type=>'v'},
-  'time' => {:offset=>138, :type=>'V'}          # unknown resolution, unknown epoche (probably seconds since beginning of day 
+  'blockSize' => {:offset=>26, :type=>'v', :len=>2},
+#  'lastBlockSize' => {:offset=>28, :type=>'v', :len=>2},
+#  'channel' => {:offset=>30, :type=>'v', :len=>2},
+#  'packetSize' => {:offset=>32, :type=>'v', :len=>2},
+#  'frameIndex' => {:offset=>34, :type=>'V', :len=>4},
+#  'upperLimit' => {:offset=>38, :type=>'e', :len=>4},
+#  'lowerLimit' => {:offset=>42, :type=>'e', :len=>4},
+#  'frequency' => {:offset=>51, :type=>'C', :len=>1},
+#  'time1' => {:offset=>58, :type=>'V', :len=>4}          # unknown resolution, unknown epoche
+  'waterDepthFt' => {:offset=>62, :type=>'e', :len=>4},    # in feet
+#  'keelDepthFt' => {:offset=>66, :type=>'e', :len=>4},    # in feet
+#  'speedGpsKnots' => {:offset=>98, :type=>'e', :len=>4},  # in knots
+#  'temperature' => {:offset=>102, :type=>'e', :len=>4},    # in °C
+  'lowrance_longitude' => {:offset=>106, :type=>'V', :len=>4},    # Lowrance encoding (easting)
+  'lowrance_latitude' => {:offset=>110, :type=>'V', :len=>4},     # Lowrance encoding (northing)
+#  'speedWaterKnots' => {:offset=>114, :type=>'e', :len=>4},   # from "water wheel sensor" if present, else GPS value(?)
+#  'courseOverGround' => {:offset=>118, :type=>'e', :len=>4},  # ourseOverGround in radians
+#  'altitudeFt' => {:offset=>122, :type=>'e', :len=>4},   # in feet
+#  'heading' => {:offset=>126, :type=>'e', :len=>4},      # in radians
+#  'flags' => {:offset=>130, :type=>'v', :len=>2},
+#  'time' => {:offset=>138, :type=>'V', :len=>4}          # unknown resolution, unknown epoche
 }
 
+f_raw = File.open("#{ARGV[0]}_output_raw.csv",'w')
 
-while output.length<20 && block_offset<s.length do
+begin
+output = []
+output_h = {}
+
+open ARGV[0], 'r' do |f|
+#  f.seek 10 # Header
+#  s = f.read 32
+#end
+
+while block_offset<File.size(ARGV[0]) do
   h={}
+  #b_sz = h['blockSize'] = s[block_offset+block_def['blockSize'][:offset], block_offset+block_def['blockSize'][:offset]+block_def['blockSize'][:len]].unpack(block_def['blockSize'][:type]).first
+	#f.seek(block_offset+block_def['blockSize'][:offset])
+	#b_sz = h['blockSize'] = f.read(block_def['blockSize'][:offset]+block_def['blockSize'][:len]].unpack(block_def['blockSize'][:type]).first
+	#ss = s[block_offset, block_offset+b_sz]
   block_def.each do |key,bdef|
-    h[key] = s[block_offset+bdef[:offset], block_offset+bdef[:offset]+4].unpack(bdef[:type]).first
+    f.seek(block_offset+bdef[:offset])
+    h[key] = f.read(bdef[:len]).unpack(bdef[:type]).first
   end
   # Some Conversions into non proprietary formats:
   h['longitude'] = h['lowrance_longitude']/POLAR_EARTH_RADIUS * (180/PI) if h.has_key?('lowrance_longitude')
@@ -74,27 +87,43 @@ while output.length<20 && block_offset<s.length do
   h['speedGpsKm']  = h['speedGpsKnots'] * KN2KM if h.has_key?('speedGpsKnots')
   begin
     block_offset += h['blockSize']
-  rescue 
+  rescue
     raise h.inspect
   end
-  output << h.dup
-end; puts "Found and decoded #{output.length} data blocks."
+  #output << h.dup
+  unless output_h[[h['latitude'], h['longitude']]]
+  output_h[[h['latitude'], h['longitude']]] = h['waterDepthM']
+    #f_raw.puts([h['longitude'], h['latitude'], h['waterDepthM']].join(';').gsub('.',','))
+    f_raw.puts([h['longitude'], h['latitude'], h['waterDepthM']].join(' ')+';')
+  end
+end; 
+#puts "Found and decoded #{output.length} data blocks."
+puts "Found and decoded #{output_h.keys.length} data blocks."
 
-puts output.to_yaml
-exit
 
-f=File.open("#{ARGV[0]}_output.csv", 'w')
-output2=output.map{|x| {'latitude'=>x['latitude'], 'longitude'=>x['longitude'], 'waterDepthM'=>x['waterDepthM']}}
-output2.group_by{|x| [x['latitude'],x['longitude']]}.each do |ll,data|
-  avg_depth = 0
-  data.map{|y| y['waterDepthM']}.each{|summand| avg_depth+=summand}
-  avg_depth = avg_depth / data.length
-  f.puts [ll[0], ll[1], avg_depth].join(';')
-end
+end # von open ... do |f|
 
+#puts output.to_yaml
+#puts output_h.to_yaml
+#exit
+
+#f=File.open("#{ARGV[0]}_output.csv", 'w')
+#output2=output.map{|x| {'latitude'=>x['latitude'], 'longitude'=>x['longitude'], 'waterDepthM'=>x['waterDepthM']}}
+#output2.group_by{|x| [x['latitude'],x['longitude']]}.each do |ll,data|
+#  avg_depth = 0
+#  data.map{|y| y['waterDepthM']}.each{|summand| avg_depth+=summand}
+#  avg_depth = avg_depth / data.length
+#  f.puts [ll[0], ll[1], avg_depth].join(';')
+#end
 #f.puts("Found #{output.length} blocks.")
 #f.puts(output.inspect);
 #f.puts('--------------- YAML ------------------')
 #f.puts(output.to_yaml);
-f.close
-
+#f.close
+rescue Exception => err
+	puts err.to_s
+	puts err.inspect	
+ensure
+ 	f_raw.close
+	puts "Read up to block_offset #{block_offset}"
+end
